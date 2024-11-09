a1e89b544653e6ffcabd2022e5bc0964e5b55529
iansani <iansani259@gmail.com>
2024-11-09 03:02:03 +0300

    Created a class for the calculator

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+ class Calculator{
+    constructor(previousOperandTextElement,currentOperandTextElement){
+        this.previousOperandTextElement=previousOperandTextElement;
+        this.currentOperandTextElement=currentOperandTextElement;
+
+    }
+ }
+ 
  const numberButtons=document.querySelectorAll('[data-number]');
  const operationsButton=document.querySelectorAll('[data-operation]');
  const equalsButton=document.querySelector('[data-equals]');
  const deleteButton=document.querySelector('[data-delete]');
  const allClearButton=document.querySelector('[data-all-clear]');
- const previousOperandText=document.querySelector('[data-previous-operand]');
- const currentOperandText=document.querySelector('[data-current-operand]');
+ const previousOperandTextElement=document.querySelector('[data-previous-operand]');
+ const currentOperandTextElement=document.querySelector('[data-current-operand]');
+
